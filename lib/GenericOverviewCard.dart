3614ce62f16d5594bf2733c1ec8df656a45be5ab
alvaroaugustodemarconeto <alvaro.marco.neto@gmail.com>
2023-06-22 09:42:17 +0200

    initial

---
+++ lib/GenericOverviewCard.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'IGenericOverviewCard.dart';
+
+class GenericOverviewCard<T extends IGenericOverviewCard> extends StatelessWidget {
+  const GenericOverviewCard({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<T>(context);
+    return Card(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            "Total horas",
+            style: TextStyle(fontSize: 22.0),
+          ),
+          Column(
+            children: [
+              Text(
+                "${provider.countItens()}",
+                style: TextStyle(fontSize: 22.0),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/IGenericOverviewCard.dart
@@ -0,0 +1,3 @@
+abstract class IGenericOverviewCard {
+  int countItens();
+}
