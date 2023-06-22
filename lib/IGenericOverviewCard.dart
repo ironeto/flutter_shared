e9a630fe99d40ecff9d2220e7921f64796ae646c
alvaroaugustodemarconeto <alvaro.marco.neto@gmail.com>
2023-06-22 10:13:07 +0200

    fix

--- lib/IGenericOverviewCard.dart
+++ lib/IGenericOverviewCard.dart
@@ -1,8 +1,5 @@
 export 'package:shared_pk/IGenericOverviewCard.dart';
 
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 abstract class IGenericOverviewCard with ChangeNotifier {
   int countItems();
 }
